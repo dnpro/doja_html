40d31408da043089c54911bdbc1760e6fd1c6f62
monodnb <monodnb@gmail.com>
2015-03-17 00:49:13 +0200

    layout 90%

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+(function ($, window, document) {
 
+    // The $ is now locally scoped 
+    $(function () {
+
+        // DOM ready!
+        //Variables
+        $document = $(document),
+            $window = $(window),
+            $body = $("body");
+
+        // Initial setup
+        
+/*        $("#ajax-content").find(".text").mCustomScrollbar({
+            autoHideScrollbar:true
+        });
+        */
+        // Event delegation
+
+    });
+
+    // Functions
+
+
+}(window.jQuery, window, document)); // Fully reference jQuery after this point.
